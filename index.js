313f84b3337beceab61089741393bc5d8551ee2b
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-12-22 11:23:11 +0600

    foods post setup complete

--- index.js
+++ index.js
@@ -9,43 +9,52 @@ app.use(cors())
 
 
 // mongodb setup//
-  
+
 const uri = `mongodb+srv://${process.env.DB_FOODUSSER}:${process.env.DB_FOODPASS}@cluster0.phy8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // backend start//
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // backend start//
 
- 
-     
-    
 
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        const database = client.db("foodsDB");
+        const foodsCollection = database.collection("foods");
+
+        // foods post//
+        app.push("/foods", async (req, res) => {
+            const foodBody = req.body
+            const result = await foodsCollection.insertOne(foodBody);
+            res.send(result)
+        })
+
+
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send("FoodFusion Website ...")
 })
 
-app.listen(port, () =>{
-    console.log("Server Runnig",port);
+app.listen(port, () => {
+    console.log("Server Runnig", port);
 })
